dd263b0f53ef6aa4ce5e81555388ea6731baef76
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-25 15:14:33 +0100

    spotted a flaw in my matches logic. means either multiples of the same teams have to be saved or scores are only saved as the last entry. going to make a league table to record wins etc

--- console.rb
+++ console.rb
@@ -17,14 +17,19 @@ AwayTeam.delete_all( runner )
 
 home_team1 = HomeTeam.new( {"name" => "Dodgy Bastards"}, runner)
 ht1 = home_team1.save()
+ht3 = home_team1.save()
+
 home_team2 = HomeTeam.new( {"name" => "Match Throwers"}, runner)
 ht2 = home_team2.save()
+ht4 = home_team2.save()
 
 away_team1 = AwayTeam.new( {"name" => "The Tossers"}, runner)
 at1 = away_team1.save()
+at3 = away_team1.save()
 
 away_team2 = AwayTeam.new( {"name" => "Big Red Balls"}, runner)
 at2 = away_team2.save()
+at4 = away_team2.save()
 
 home_team_lineup1 = HomeTeamLineup.new( {"catcher_1" => "Fingers Jones", "catcher_2" => "Immortal Jane", "corner_1" => "Big Dave", "corner_2" => "Jack Tash", "sniper_1" => "Bobby Jr Sr", "sniper_2" => "Rubbish John", "thrower_1" => "All Talk Harrison", "thrower_2" => "Magic Mike", "home_team_id" => ht1.id}, runner )
 htl1 = home_team_lineup1.save()
@@ -38,8 +43,18 @@ match1.save()
 match2 = Match.new( {"home_team_id" => ht2.id, "away_team_id" => at2.id}, runner )
 match2.save()
 
-match1.final_score(3,2)
-match2.final_score(5,9)
+match3 = Match.new( {"home_team_id" => ht3.id, "away_team_id" => at4.id}, runner )
+match3.save()
+
+match4 = Match.new( {"home_team_id" => ht4.id, "away_team_id" => at3.id}, runner )
+match4.save()
+
+match1.final_score( 3,2 )
+match2.final_score( 5,9 )
+match3.final_score( 7,8 )
+match4.final_score( 6,6 )
+
+
 
 binding.pry
 nil
