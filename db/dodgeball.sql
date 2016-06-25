7109307537be9b00c09aae3dbcec7b7770fb6222
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-25 12:53:47 +0100

    starting to add lineups

--- db/dodgeball.sql
+++ db/dodgeball.sql
@@ -1,6 +1,8 @@
 DROP TABLE matches;
 DROP TABLE home_teams;
 DROP TABLE away_teams;
+DROP TABLE home_teams_lineups;
+DROP TABLE away_teams_lineups;
 
 CREATE TABLE home_teams (
   id SERIAL4 primary key,
@@ -19,3 +21,49 @@ CREATE TABLE matches (
   home_team_id INT4 references home_teams(id),
   away_team_id INT4 references away_teams(id)
 );
+
+CREATE TABLE home_teams_lineups (
+  id SERIAL4 primary key,
+  catcher_1 VARCHAR(255) not null,
+  catcher_2 VARCHAR(255) not null,
+  corner_1 VARCHAR(255) not null,
+  corner_2 VARCHAR(255) not null,
+  sniper_1 VARCHAR(255) not null,
+  sniper_2 VARCHAR(255) not null,
+  thrower_1 VARCHAR(255) not null,
+  thrower_2 VARCHAR(255) not null,
+  home_team_id INT4 references home_team(id),
+  away_team_id INT4 references away_team(id)
+);
+
+CREATE TABLE away_teams_lineups (
+  id SERIAL4 primary key,
+  catcher_1 VARCHAR(255) not null,
+  catcher_2 VARCHAR(255) not null,
+  corner_1 VARCHAR(255) not null,
+  corner_2 VARCHAR(255) not null,
+  sniper_1 VARCHAR(255) not null,
+  sniper_2 VARCHAR(255) not null,
+  thrower_1 VARCHAR(255) not null,
+  thrower_2 VARCHAR(255) not null,
+  away_team_id INT4 references away_team(id),
+  home_team_id INT4 references home_team(id)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ models/home_team_lineup.rb
@@ -0,0 +1,60 @@
+require_relative('../db/sql_runner')
+require('pry-byebug')
+
+class HomeTeamLineup
+
+  attr_reader( :id, :catcher_1, :catcher_2, :corner_1, :corner_2, :sniper_1, :sniper_2, :thrower_1, :thrower_2 )
+
+  def initialize( options, runner )
+    @id = options['id'].to_i
+    @catcher_1 = options['catcher_1']
+    @catcher_2 = options['catcher_2']
+    @corner_1 = options['corner_1']
+    @corner_2 = options['corner_2']
+    @sniper_1 = options['sniper_1']
+    @sniper_2 = options['sniper_2']
+    @thrower_1 = options['thrower_1']
+    @thrower_2 = options['thrower_2']
+    @runner = runner
+  end
+
+  def save()
+    sql = "INSERT INTO home_teams_lineups (catcher_1, catcher_2, corner_1, corner_2, sniper_1, sniper_2, thrower_1, thrower_2) VALUES ('#{catcher_1}', '#{catcher_2}', '#{corner_1}', '#{corner_2}', '#{sniper_1}', '#{sniper_2}', '#{thrower_1}', '#{thrower_2}') RETURNING *"
+    return HomeTeamLineup.map_item( sql, @runner )
+  end
+
+  def self.all( runner )
+    sql = "SELECT * FROM home_teams_lineups"
+    return HomeTeamLineup.map_items( sql, runner )
+  end
+
+  def self.delete_all( runner )
+    sql = "DELETE FROM home_teams_lineups"
+    runner.run( sql )
+  end
+
+  def opponents()
+    sql = "SELECT a.* FROM away_teams_lineups a INNER JOIN matches m on m.away_team_id = a.id WHERE home_team_id = #{@id}"
+    return AwayTeamLineup.map_items( sql, @runner )
+  end
+
+  def self.map_items( sql, runner )
+    home_teams_lineups = runner.run( sql )
+    result = home_teams_lineups.map { |home_team_lineup| HomeTeamLineup.new( home_team_lineup, runner ) }
+    return result
+  end
+
+  def self.map_item( sql, runner )
+    result - HomeTeamLineup.map_items( sql, runner )
+    return result.first
+  end
+
+end
+
+
+
+
+
+
+
+
